1a6d0cc4f29d6eb5ece5591f9784dff04b27e233
karate_guy <fabianstachurski2010@gmail.com>
2026-09-07 17:08:12 +0200

    Add abstract Task class with properties and methods
    Implemented an abstract Task class with title, creation date, and completion status.

--- STACHURSKI_FABIAN/07_09_2026/Tasks.py
+++ STACHURSKI_FABIAN/07_09_2026/Tasks.py
@@ -1,1 +1,19 @@
-coś
+from abc import ABC, abstract method
+from datetime import datetime
+
+# a)
+
+class Task(ABC):
+  def__init__(self, title):
+    self.title = title
+    self.created_at = datetime.now()
+    self._done = False
+
+  @property
+def done(self):
+  return self._done
+def complete(self):
+  self._done = True
+@abstractmethod
+def estimate_minutes(self):
+  pass
