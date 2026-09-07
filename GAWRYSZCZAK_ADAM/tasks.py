412bb1cc211c6cf14c6d11645a5a0393160dacf2
gawryszczakadam10-create <gawryszczakadam10@gmail.com>
2026-09-07 16:36:43 +0200

    podpunkt a

--- GAWRYSZCZAK_ADAM/tasks.py
+++ GAWRYSZCZAK_ADAM/tasks.py
@@ -1,1 +1,21 @@
+# a)
+from abc import ABC, abstractmethod
+from datetime import datetime
 
+
+class Task(ABC):
+    def __init__(self, title, created_at=None):
+        self.title = title
+        self.created_at = created_at if created_at else datetime.now()
+        self._done = False
+
+    @abstractmethod
+    def estimate_minutes(self):
+        pass
+
+    def complete(self):
+        self._done = True
+
+    @property
+    def done(self):
+        return self._done
