710200888c40a0e270d3b706a660cb6d0ba90b55
gawryszczakadam10-create <gawryszczakadam10@gmail.com>
2026-09-07 17:09:41 +0200

    Add SimpleTask, TimedTask, and RecurringTask classes

--- GAWRYSZCZAK_ADAM/tasks.py
+++ GAWRYSZCZAK_ADAM/tasks.py
@@ -15,3 +15,35 @@ class Task(ABC):
     @property
     def done(self):
         return self._done
+#b)
+class SimpleTask(Task):
+    def __init__(self, title, minutes):
+        super().__init__(title)
+        self.minutes = minutes
+    def estimate_minutes(Self):
+        return self.minutes
+class TimedTask(Task):
+    def __init__(self,title,minutes):
+        super().__init__(title)
+        self.minutes = minutes
+    def estimate_minutes(self)
+        return self.minutes
+class RecurringTask(TimedTask):
+    def __init__(Self, title, minutes, times_per_week):
+        super().__init__(title,minutes)
+        self.times_per_week = times_per_week
+    def estimate_minutes(Self):
+        return self.minutes * self.times_per_week
+
+
+t1 = SimpleTask("Nauka", 30)
+t2 = TimedTask("Trening", 60)
+t3 = RecurringTask("Python", 20, 3)
+
+t1.complete()
+
+lista = TaskList()
+
+lista.add(1)
+lista.add(2)
+lista.add(3)
