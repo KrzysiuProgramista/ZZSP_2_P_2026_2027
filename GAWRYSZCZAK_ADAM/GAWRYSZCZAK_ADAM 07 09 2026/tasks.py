1cf7a5196ea7068a596372b105169bce092ab957
gawryszczakadam10-create <gawryszczakadam10@gmail.com>
2026-09-07 16:55:31 +0200

    Add files via upload

---
+++ GAWRYSZCZAK_ADAM/GAWRYSZCZAK_ADAM 07 09 2026/tasks.py
@@ -0,0 +1,18 @@
+#a)
+from abc import ABC, abstractmethod
+
+class Task(ABC):
+    def __init__(self, title):
+        self.title = title
+        self.created_at = None
+        self.done = False
+
+    @abstracmethod
+    def estimate_minutes(self):
+        pass
+    def complete(self):
+        self._done = True
+    @property
+    def done(self):
+        return self._done
+    
