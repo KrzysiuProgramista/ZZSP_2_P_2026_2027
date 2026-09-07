71f38f5d9c2be88cb5d14d3fbc2503d77e14ebab
gawryszczakadam10-create <gawryszczakadam10@gmail.com>
2026-09-07 17:03:18 +0200

    Update tasks.py

--- GAWRYSZCZAK_ADAM/GAWRYSZCZAK_ADAM 07 09 2026/tasks.py
+++ GAWRYSZCZAK_ADAM/GAWRYSZCZAK_ADAM 07 09 2026/tasks.py
@@ -15,4 +15,21 @@ class Task(ABC):
     @property
     def done(self):
         return self._done
-    
+#b)
+class SimpleTask(Task):
+    def __init__(self, title, minutes):
+        super().__init__(title)
+        self.minutes = minutes
+    def estimate_minutes(Self):
+        return self.minutes
+class TimedTask(Task):
+    def __init__(self,title,minutes):
+        super().__init__(title)
+        self.minutes = minutes
+    def estimate_minutes(self)
+        return self.minutes
+class RecurringTask(TimedTask):
+    def __init__(Self, title, minutes, times_per_week):
+        super().__init__(title,minutes)
+        self.times_per_week = times_per_week
+    def 
